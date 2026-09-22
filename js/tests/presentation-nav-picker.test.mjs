e7b9aecef92f520c68deeee12f0e025fb25c218b
Mark Counterman <ilovepoker@gmail.com>
2026-09-21 21:49:55 -0500

    feat: reconcile center and hamburger labels onto current main (#23)
    * nav: reconcile current-main labels js/dist/forum.js
    
    * nav: reconcile current-main labels js/dist/forum.js.map
    
    * nav: reconcile current-main labels js/src/forum/index.js
    
    * nav: reconcile current-main labels js/src/forum/utils/discussionBrandTitle.js
    
    * nav: reconcile current-main labels js/src/forum/utils/presentationTitle.js
    
    * nav: reconcile current-main labels js/tests/discussion-center-menu.test.mjs
    
    * nav: reconcile presentation picker tests
    
    * nav: add js/tests/drawer-home.test.mjs
    
    * nav: add resources/less/drawer-home.less
    
    * nav: register phone drawer home styling

--- js/tests/presentation-nav-picker.test.mjs
+++ js/tests/presentation-nav-picker.test.mjs
@@ -40,6 +40,9 @@ test('phone hamburger drawer mounts presentation nav via HeaderSecondary', () =>
   assert.match(indexSrc, /from 'flarum\/forum\/components\/HeaderSecondary'/);
   assert.match(indexSrc, /extend\(HeaderSecondary\.prototype, 'items'/);
   assert.match(indexSrc, /flatrateDrawerNav/);
+  assert.match(indexSrc, /flatrateDrawerHome/);
+  assert.match(indexSrc, /fa-warehouse/);
+  assert.match(indexSrc, />\s*HOME\s*<\/LinkButton>/);
   assert.match(indexSrc, /flatrateDrawerFollowing/);
   assert.match(indexSrc, /flatrateDrawerStart/);
   assert.match(indexSrc, /pushToStartHref/);
