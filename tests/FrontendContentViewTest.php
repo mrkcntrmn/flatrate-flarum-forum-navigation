7e13f8734ae70e6af75e0587554afbbc2c71fef7
Mark Counterman <ilovepoker@gmail.com>
2026-09-24 16:09:08 -0500

    sync: preserve approved maintenance banner resolution for tests/FrontendContentViewTest.php

--- tests/FrontendContentViewTest.php
+++ tests/FrontendContentViewTest.php
@@ -69,8 +69,10 @@ class FrontendContentViewTest extends TestCase
         $less = (string) file_get_contents($root . '/resources/less/forum.less');
 
         $this->assertStringContainsString('id="flatrate-maintenance-banner"', $view);
+        $this->assertStringContainsString('FlatRateMaintenanceBanner-copy', $view);
         $this->assertStringContainsString('FLATRATE.WIKI is undergoing maintenance.', $view);
-        $this->assertStringContainsString('4:00 PM, 9/24/26', $view);
+        $this->assertStringContainsString('FlatRateMaintenanceBanner-schedule', $view);
+        $this->assertStringContainsString('9/24/26 4:00 PM', $view);
         $this->assertStringContainsString('flatrate:maintenance-banner:2026-09-24-1600', $view);
         $this->assertStringContainsString("window.localStorage.setItem(storageKey, 'dismissed')", $view);
         $this->assertStringContainsString("drawer.parentNode.insertBefore(banner, drawer.nextSibling)", $view);
