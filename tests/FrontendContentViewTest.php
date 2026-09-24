9549e26a144052bc8cf1cf8eb5c96da375df92cf
Mark Counterman <ilovepoker@gmail.com>
2026-09-24 16:05:40 -0500

    test: carry 9/24 maintenance timestamp contract

--- tests/FrontendContentViewTest.php
+++ tests/FrontendContentViewTest.php
@@ -72,8 +72,8 @@ class FrontendContentViewTest extends TestCase
         $this->assertStringContainsString('FlatRateMaintenanceBanner-copy', $view);
         $this->assertStringContainsString('FLATRATE.WIKI is undergoing maintenance.', $view);
         $this->assertStringContainsString('FlatRateMaintenanceBanner-schedule', $view);
-        $this->assertStringContainsString('9/23/26 4:00 PM', $view);
-        $this->assertStringContainsString('flatrate:maintenance-banner:2026-09-23-1600', $view);
+        $this->assertStringContainsString('9/24/26 4:00 PM', $view);
+        $this->assertStringContainsString('flatrate:maintenance-banner:2026-09-24-1600', $view);
         $this->assertStringContainsString("window.localStorage.setItem(storageKey, 'dismissed')", $view);
         $this->assertStringContainsString("drawer.parentNode.insertBefore(banner, drawer.nextSibling)", $view);
         $this->assertStringContainsString('aria-label="Dismiss maintenance announcement"', $view);
